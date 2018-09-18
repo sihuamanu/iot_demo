21ea3b58561b1059141253e432f51192b94302d3
root <you@example.com>
2018-09-17 21:14:47 -0700

    edit for spark2

--- total_event_count.py
+++ total_event_count.py
@@ -7,7 +7,7 @@ from pyspark.streaming.kafka import KafkaUtils
 from pyspark.storagelevel import StorageLevel
 from pyspark.sql import SQLContext
 
-conf = (SparkConf().setMaster("yarn-client").setAppName("Total Event Count"))
+conf = (SparkConf().setAppName("Total Event Count"))
 sc = SparkContext(conf = conf)
 sqc = SQLContext(sc)
 
@@ -15,14 +15,14 @@ Config = ConfigParser.ConfigParser()
 Config.read('particlespark.conf')
 kudu_master = Config.get('Kudu','KuduMaster')
 
-kudu_all_events_table = "particle_test"
-kudu_counts_table = "particle_counts_total"
+kudu_all_events_table = "impala::default.particle_test"
+kudu_counts_table = "impala::default.particle_counts_total"
 
 #Read from Kudu table particle_test
 kudu_events_df = sqc.read.format('org.apache.kudu.spark.kudu').option('kudu.master',kudu_master).option('kudu.table',kudu_all_events_table).load()
 
 #Grab only the event column, split it by white space and count up each unique key
-kudu_events_count = kudu_events_df.map(lambda x: x.event).flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
+kudu_events_count = kudu_events_df.rdd.map(lambda x: x.event).flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
 
 #Convert event counts to DataFrame from RDD
 kudu_events_count_df = kudu_events_count.toDF(['event_word','count'])
